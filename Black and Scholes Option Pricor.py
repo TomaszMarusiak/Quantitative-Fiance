490a273e320c1d7f0f75d907a661fa3d9f0e00cd
TomaszMarusiak <124789923+TomaszMarusiak@users.noreply.github.com>
2023-02-08 14:10:49 +0000

    Add files via upload
    Black and Scholes Option Pricer.
    
    Simple function capturing fair value of an European option contract considering Strike Price, Spot Price, Risk-free rate, time to maturity and volatility in pair with Black and Scholes Theorem

---
+++ Black and Scholes Option Pricor.py
@@ -0,0 +1,51 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC Black and Scholes Option Pricor
+
+# COMMAND ----------
+
+#import packages
+
+import numpy as np
+from scipy.stats import norm
+
+# COMMAND ----------
+
+#Variables
+"""
+k = strike price
+st = spot price
+r = risk free rate
+t = maturity
+vol = volitality
+"""
+
+k = 350
+st = 360
+r = 0.0329
+t = 240/365
+vol = 0.30
+
+# COMMAND ----------
+
+#create a function, type c=call, p=put
+def BSM(st,k,r,t,vol, type="C"):
+  d1 = (np.log(st/k) + (r+(vol**2/2))*t)/(vol*np.sqrt(t))
+  d2 = d1 - vol*np.sqrt(t)
+  try:
+    if type == "C":
+      price = norm.cdf(d1,0,1)*st - norm.cdf(d2,0,1)*k*np.exp(-r*t)
+    elif type == "P":
+      price = -st*norm.cdf(-d1,0,1) + norm.cdf(-d2,0,1)*k*np.exp(-r*t)
+  except:
+    print('Check the type')
+    
+  print('Black and Scholes fair price is: ',price)
+
+# COMMAND ----------
+
+BSM(600,599.4,0.0077,115/365,0.1697, type="C")
+
+# COMMAND ----------
+
+
